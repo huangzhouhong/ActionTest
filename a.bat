1d13ce630a63e0341bff9b506f90b6238964275e
huangzhouhong <778612084@qq.com>
2024-09-13 11:41:32 +0800

    Update a.bat

--- a.bat
+++ a.bat
@@ -2,7 +2,7 @@ echo in bat
 
 set "activate=C:\Miniconda\Scripts\activate.bat"
 call %activate%
-conda create --name browser python=3.9 --yes
+call conda create --name browser python=3.9 --yes
 call %activate% browser
 
 echo show version
